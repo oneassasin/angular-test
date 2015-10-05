157fce66ff3c29ab9bb30011a7fa8e05bb626c81
oneassasin <oneassasin@rambler.ru>
2015-10-05 05:19:43 +0300

    +  taskController: updateTask +  taskController: deleteTask +  taskController: getTask

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -28,11 +28,80 @@ module.exports.createTask = function(req, res, next) {
 };
 
 module.exports.updateTask = function(req, res, next) {
+  req.assert('text').notEmpty();
+  req.asyncValidationErrors()
+      .catch(function(errors) {
+        errors.status = 400;
+        return Promise.reject(errors);
+      })
+      .then(function() {
+        return promising.parsePromise(Parse.User.currentAsync());
+      })
+      .then(function(user) {
+        const taskID = req.sanitize('id').escape();
+        const query = new Parse.Query(ToDo);
+        query.equalTo('objectId', taskID);
+        query.equalTo('author', user.id);
+        return promising.parsePromise(query.first());
+      })
+      .then(function(task) {
+        const text = req.sanitize('text').escape();
+        task.set('text', text);
+        return promising.parsePromise(task.save(null));
+      })
+      .then(function(task) {
+        res.status(200).header('Location', task.id).end();
+      })
+      .catch(function(error) {
+        error.status = 404;
+        return next(error);
+      });
 };
 
 module.exports.deleteTask = function(req, res, next) {
+  promising.parsePromise(Parse.User.currentAsync())
+      .then(function(user) {
+        const taskID = req.sanitize('id').escape();
+        const query = new Parse.Query(ToDo);
+        query.equalTo('objectId', taskID);
+        query.equalTo('author', user.id);
+        return promising.parsePromise(query.first());
+      })
+      .then(function(task) {
+        if (!task) {
+          const error = new Error();
+          error.status = 404;
+          error.message = 'task_not_found';
+          return Promise.reject(error);
+        }
+
+        return promising.parsePromise(task.destroy(null));
+      })
+      .then(function() {
+        res.status(200).end();
+      })
+      .catch(next);
 };
 
 module.exports.getTask = function(req, res, next) {
+  promising.parsePromise(Parse.User.currentAsync())
+      .then(function(user) {
+        const taskID = req.sanitize('id').escape();
+        const query = new Parse.Query(ToDo);
+        query.equalTo('objectId', taskID);
+        query.equalTo('author', user.id);
+        return promising.parsePromise(query.first());
+      })
+      .then(function(task) {
+        if (!task) {
+          const error = new Error();
+          error.status = 404;
+          error.message = 'task_not_found';
+          return Promise.reject(error);
+        }
+
+        res.status(200).send(task).end();
+      })
+      .catch(next);
 };
 
